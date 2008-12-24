c583eab5283290fdb17f0b8efd1bf11a09ca29cb
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2008-12-24 02:24:56 +0000

    Removed unused macros from color.h.
    git-svn-id: https://rawstudio.org/svn/rawstudio/branches/rawstudio-plugins@2087 ff961bb0-e316-0410-bf66-ce93e2ff3b84

--- src/color.h
+++ src/color.h
@@ -30,89 +30,14 @@
 
 #define GAMMA 2.2 /* this is ONLY used to render the histogram */
 
-#define _MAX(in, max) if (in>max) max=in
-
-#ifdef __i386__
-#define _MAX_CMOV(in, max) \
-asm volatile (\
-	"cmpl	%1, %0\n\t"\
-	"cmovl	%1, %0\n\t"\
-	:"+r" (max)\
-	:"r" (in)\
-)
-#endif
-
 #define _CLAMP(in, max) if (in>max) in=max
 
-#ifdef __i386__
-#define _CLAMP_CMOV(in, max) \
-asm volatile (\
-	"cmpl	%0, %1\n\t"\
-	"cmovl	%1, %0\n\t"\
-	:"+r" (in)\
-	:"r" (max)\
-)
-#endif
-
 #define _CLAMP65535(a) a = MAX(MIN(65535,a),0)
-
-#ifdef __i386__
-#define _CLAMP65535_CMOV(value) \
-asm volatile (\
-	"xorl %%ecx, %%ecx\n\t"\
-	"cmpl %%ecx, %0\n\t"\
-	"cmovl %%ecx, %0\n\t"\
-	"movl	$65535, %%ecx\n\t"\
-	"cmpl %0, %%ecx\n\t"\
-	"cmovl %%ecx, %0\n\t"\
-	:"+r" (value)\
-	:\
-	:"%ecx"\
-)
-#endif
 
 #define _CLAMP65535_TRIPLET(a, b, c) \
 a = MAX(MIN(65535,a),0);b = MAX(MIN(65535,b),0);c = MAX(MIN(65535,c),0)
 
-#if defined (__i386__) || defined (__x86_64__)
-#define _CLAMP65535_TRIPLET_CMOV(a, b, c) \
-asm volatile (\
-	"xor  %%"REG_c", %%"REG_c"\n\t"\
-	"cmp  %%"REG_c", %0\n\t"\
-	"cmovl %%"REG_c", %0\n\t"\
-	"cmp %%"REG_c", %1\n\t"\
-	"cmovl %%"REG_c", %1\n\t"\
-	"cmp %%"REG_c", %2\n\t"\
-	"cmovl %%"REG_c", %2\n\t"\
-	"mov $0xffff, %%"REG_c"\n\t"\
-	"cmp %%"REG_c", %0\n\t"\
-	"cmovg %%"REG_c", %0\n\t"\
-	"cmp %%"REG_c", %1\n\t"\
-	"cmovg %%"REG_c", %1\n\t"\
-	"cmp %%"REG_c", %2\n\t"\
-	"cmovg %%"REG_c", %2\n\t"\
-	:"+r" (a), "+r" (b), "+r" (c)\
-	:\
-	:"%"REG_c\
-)
-#endif
-
 #define _CLAMP255(a) a = MAX(MIN(255,a),0)
-
-#ifdef __i386__
-#define _CLAMP255_CMOV(value) \
-asm volatile (\
-	"xorl %%ecx, %%ecx\n\t"\
-	"cmpl %%ecx, %0\n\t"\
-	"cmovl %%ecx, %0\n\t"\
-	"movl	$255, %%ecx\n\t"\
-	"cmpl %0, %%ecx\n\t"\
-	"cmovl %%ecx, %0\n\t"\
-	:"+r" (value)\
-	:\
-	:"%ecx"\
-)
-#endif
 
 #define COLOR_BLACK(c) do { c.red=0; c.green=0; c.blue=0; } while (0)
 
