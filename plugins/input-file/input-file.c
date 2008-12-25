cedfb6006c9b39e226f785e018233e2beb5f1918
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2008-12-25 19:59:44 +0000

    Added RSInputFile plugin.
    git-svn-id: https://rawstudio.org/svn/rawstudio/branches/rawstudio-plugins@2124 ff961bb0-e316-0410-bf66-ce93e2ff3b84

---
+++ plugins/input-file/input-file.c
@@ -0,0 +1,194 @@
+/*
+ * Copyright (C) 2006-2008 Anders Brander <anders@brander.dk> and 
+ * Anders Kvist <akv@lnxbx.dk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <rawstudio.h>
+
+#define RS_TYPE_INPUT_FILE (rs_input_file_type)
+#define RS_INPUT_FILE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), RS_TYPE_INPUT_FILE, RSInputFile))
+#define RS_INPUT_FILE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), RS_TYPE_INPUT_FILE, RSInputFileClass))
+#define RS_IS_INPUT_FILE(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), RS_TYPE_INPUT_FILE))
+
+typedef struct _RSInputFile RSInputFile;
+typedef struct _RSInputFileClass RSInputFileClass;
+
+struct _RSInputFile {
+	RSFilter parent;
+
+	gchar *filename;
+	RS_IMAGE16 *image;
+};
+
+struct _RSInputFileClass {
+	RSFilterClass parent_class;
+};
+
+enum {
+	PROP_0,
+	PROP_FILENAME
+};
+
+static GType rs_input_file_get_type (GTypeModule *module);
+static void rs_input_file_class_init(RSInputFileClass *klass);
+static void rs_input_file_init(RSInputFile *filter);
+static void get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
+static void set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
+static RS_IMAGE16 *get_image(RSFilter *filter);
+static gint get_width(RSFilter *filter);
+static gint get_height(RSFilter *filter);
+
+static GType rs_input_file_type = 0;
+
+static RSFilterClass *rs_input_file_parent_class = NULL;
+
+G_MODULE_EXPORT void
+rs_plugin_load(RSPlugin *plugin)
+{
+	g_debug("rs_plugin_load(%p)", plugin);
+	g_debug("1");
+	rs_input_file_get_type(G_TYPE_MODULE(plugin));
+	g_debug("2");
+}
+
+static GType
+rs_input_file_get_type (GTypeModule *module)
+{
+	g_debug("rs_input_file_get_type(%p)", module);
+	if (!rs_input_file_type)
+	{
+		static const GTypeInfo filter_info =
+		{
+			sizeof (RSInputFileClass),
+			(GBaseInitFunc) NULL,
+			(GBaseFinalizeFunc) NULL,
+			(GClassInitFunc) rs_input_file_class_init,
+			NULL,           /* class_finalize */
+			NULL,           /* class_data     */
+			sizeof (RSInputFile),
+			0,              /* n_preallocs    */
+			(GInstanceInitFunc) rs_input_file_init
+		};
+
+		rs_input_file_type = g_type_module_register_type(
+			module,
+			RS_TYPE_FILTER,
+			"RSInputFile",
+			&filter_info,
+			0);
+	}
+	g_debug("rs_input_file_type = %d", (int) rs_input_file_type);
+	return rs_input_file_type;
+}
+
+static void
+rs_input_file_class_init (RSInputFileClass *klass)
+{
+	g_debug("rs_input_file_class_init(%p)", klass);
+	RSFilterClass *filter_class = RS_FILTER_CLASS (klass);
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+	rs_input_file_parent_class = g_type_class_peek_parent (klass);
+
+	object_class->get_property = get_property;
+	object_class->set_property = set_property;
+
+	g_object_class_install_property(object_class,
+		PROP_FILENAME, g_param_spec_string (
+			"filename",
+			"Filename",
+			"The filename of the file to open",
+			NULL,
+			G_PARAM_READWRITE)
+	);
+
+	filter_class->name = "File loader based on rs_filetypes";
+	filter_class->get_image = get_image;
+	filter_class->get_width = get_width;
+	filter_class->get_height = get_height;
+}
+
+static void
+rs_input_file_init (RSInputFile *filter)
+{
+	g_debug("rs_input_file_init(%p)", filter);
+
+	filter->filename = NULL;
+	filter->image = NULL;
+}
+
+static void
+get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+{
+	switch (property_id)
+	{
+		case PROP_FILENAME:
+			g_value_get_string (value);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
+}
+
+static void
+set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
+{
+	RSInputFile *input = RS_INPUT_FILE(object);
+	switch (property_id)
+	{
+		case PROP_FILENAME:
+			g_debug("set filename = \"%s\"", g_value_dup_string (value));
+			g_free (input->filename);
+			input->filename = g_value_dup_string (value);
+			if (input->image)
+				g_object_unref(input->image);
+			input->image = rs_filetype_load(input->filename, FALSE);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
+}
+
+static RS_IMAGE16 *
+get_image(RSFilter *filter)
+{
+	RSInputFile *input = RS_INPUT_FILE(filter);
+
+	return input->image;
+}
+
+static gint
+get_width(RSFilter *filter)
+{
+	RSInputFile *input = RS_INPUT_FILE(filter);
+
+	if (input->image)
+		return input->image->w;
+	else
+		return -1;
+}
+
+static gint
+get_height(RSFilter *filter)
+{
+	RSInputFile *input = RS_INPUT_FILE(filter);
+
+	if (input->image)
+		return input->image->h;
+	else
+		return -1;
+}
