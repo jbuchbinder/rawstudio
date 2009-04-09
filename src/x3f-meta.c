b5d384b658a7eeb703c96667c0ff87d7054d3b3f
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2009-04-09 11:52:12 +0000

    Backported revision 2173 from trunk.
    git-svn-id: https://rawstudio.org/svn/rawstudio/branches/rawstudio-1.2@2327 ff961bb0-e316-0410-bf66-ce93e2ff3b84

--- src/x3f-meta.c
+++ src/x3f-meta.c
@@ -136,6 +136,8 @@ rs_x3f_load_meta(const gchar *filename, RS_METADATA *meta)
 	raw_get_uint(rawfile, G_STRUCT_OFFSET(X3F_FILE, rotation), &file.rotation);
 	raw_get_uint(rawfile, raw_get_filesize(rawfile)-4, &file.directory_start);
 
+	meta->orientation=file.rotation;
+
 	if ((file.version_major == 2) && (file.version_minor == 2))
 	{
 		/* Copy all data types in one go */
@@ -316,6 +318,12 @@ rs_x3f_load_thumb(const gchar *src)
 
 	if (pixbuf)
 	{
+		if (file.rotation > 0)
+		{
+			pixbuf2 = gdk_pixbuf_rotate_simple(pixbuf,360-file.rotation);
+			g_object_unref(pixbuf);
+			pixbuf = pixbuf2;
+		}
 		ratio = ((gdouble) gdk_pixbuf_get_width(pixbuf))/((gdouble) gdk_pixbuf_get_height(pixbuf));
 		if (ratio>1.0)
 			pixbuf2 = gdk_pixbuf_scale_simple(pixbuf, 128, (gint) (128.0/ratio), GDK_INTERP_BILINEAR);
