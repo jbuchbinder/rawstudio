62a8902c05081b1647ff8574e5b54ca80cd3b8da
post <post@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2010-01-23 13:57:11 +0000

    Colorspace transform. Add multithreading, ROI and SSE2 code to all internal 16 bit -> 8 bit conversions. Note that the GdkPixbuffer now has an alpha channel, which might all to other code changes needed.
    git-svn-id: https://rawstudio.org/svn/rawstudio/branches/rawstudio-ng-color@3057 ff961bb0-e316-0410-bf66-ce93e2ff3b84

--- plugins/colorspace-transform/colorspace_transform.c
+++ plugins/colorspace-transform/colorspace_transform.c
@@ -31,6 +31,23 @@
 typedef struct _RSColorspaceTransform RSColorspaceTransform;
 typedef struct _RSColorspaceTransformClass RSColorspaceTransformClass;
 
+typedef struct {
+	RSColorspaceTransform *cst;
+	GThread *threadid;
+	gint start_x;
+	gint start_y;
+	gint end_x;
+	gint end_y;
+	RS_IMAGE16 *input;
+	void *output;
+	RSColorSpace *input_space;
+	RSColorSpace *output_space;
+	RS_MATRIX3 *matrix;
+	gboolean gamma_correct;
+	guchar* table8;
+	gfloat output_gamma;
+} ThreadInfo;
+
 struct _RSColorspaceTransform {
 	RSFilter parent;
 
@@ -51,9 +68,14 @@ enum {
 static RSFilterResponse *get_image(RSFilter *filter, const RSFilterRequest *request);
 static RSFilterResponse *get_image8(RSFilter *filter, const RSFilterRequest *request);
 static gboolean convert_colorspace16(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *input_image, RS_IMAGE16 *output_image, RSColorSpace *input_space, RSColorSpace *output_space);
-static void convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *input_image, GdkPixbuf *output_image, RSColorSpace *input_space, RSColorSpace *output_space);
+static void convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *input_image, GdkPixbuf *output_image, RSColorSpace *input_space, RSColorSpace *output_space, GdkRectangle *roi);
 
 static RSFilterClass *rs_colorspace_transform_parent_class = NULL;
+
+/* SSE2 optimized functions */
+extern void transform8_srgb_sse2(ThreadInfo* t);
+extern void transform8_otherrgb_sse2(ThreadInfo* t);
+extern gboolean cst_has_sse2();
 
 G_MODULE_EXPORT void
 rs_plugin_load(RSPlugin *plugin)
@@ -106,8 +128,6 @@ get_image(RSFilter *filter, const RSFilterRequest *request)
 		if (convert_colorspace16(colorspace_transform, input, output, input_space, output_space))
 		{
 			/* Image was converted */
-			printf("\033[33m16 input_space: %s\033[0m\n", (input_space) ? G_OBJECT_TYPE_NAME(input_space) : "none");
-			printf("\033[33m16 output_space: %s\n\033[0m", (output_space) ? G_OBJECT_TYPE_NAME(output_space) : "none");
 			response = rs_filter_response_clone(previous_response);
 			g_object_unref(previous_response);
 			rs_filter_response_set_image(response, output);
@@ -137,53 +157,60 @@ get_image8(RSFilter *filter, const RSFilterRequest *request)
 	RSFilterResponse *response;
 	RS_IMAGE16 *input;
 	GdkPixbuf *output = NULL;
+	GdkRectangle *roi;
 
 	previous_response = rs_filter_get_image(filter->previous, request);
 	input = rs_filter_response_get_image(previous_response);
 	if (!RS_IS_IMAGE16(input))
 		return previous_response;
 
+	roi = rs_filter_request_get_roi(request);
 	RSColorSpace *input_space = rs_filter_param_get_object_with_type(RS_FILTER_PARAM(previous_response), "colorspace", RS_TYPE_COLOR_SPACE);
 	RSColorSpace *output_space = rs_filter_param_get_object_with_type(RS_FILTER_PARAM(request), "colorspace", RS_TYPE_COLOR_SPACE);
 
+#if 0
 	printf("\033[33m8 input_space: %s\033[0m\n", (input_space) ? G_OBJECT_TYPE_NAME(input_space) : "none");
 	printf("\033[33m8 output_space: %s\n\033[0m", (output_space) ? G_OBJECT_TYPE_NAME(output_space) : "none");
+#endif
 
 	response = rs_filter_response_clone(previous_response);
 	g_object_unref(previous_response);
-	output = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, input->w, input->h);
+	output = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, input->w, input->h);
 
-	convert_colorspace8(colorspace_transform, input, output, input_space, output_space);
+	/* Process output */
+	convert_colorspace8(colorspace_transform, input, output, input_space, output_space, roi);
 
 	rs_filter_response_set_image8(response, output);
 	g_object_unref(output);
-
-	/* Process output */
-
 	g_object_unref(input);
 	return response;
 }
 
 static void
-transform8_c(RS_IMAGE16 *input, GdkPixbuf *output, RS_MATRIX3 *matrix, guchar *table8)
+transform8_c(ThreadInfo* t)
 {
 	gint row;
 	gint r,g,b;
 	gint width;
 	RS_MATRIX3Int mati;
-
+	RS_IMAGE16 *input = t->input;
+	GdkPixbuf *output = (GdkPixbuf *)t->output;
+	guchar *table8 = t->table8;
+	
+	g_assert(RS_IS_IMAGE16(input));
+	g_assert(GDK_IS_PIXBUF(output));
 	gint o_channels = gdk_pixbuf_get_n_channels(output);
 
-	matrix3_to_matrix3int(matrix, &mati);
+	matrix3_to_matrix3int(t->matrix, &mati);
 
-	for(row=0 ; row<input->h ; row++)
+	for(row=t->start_y ; row<t->end_y ; row++)
 	{
-		gushort *i = GET_PIXEL(input, 0, row);
-		guchar *o = GET_PIXBUF_PIXEL(output, 0, row);
+		gushort *i = GET_PIXEL(input, t->start_x, row);
+		guchar *o = GET_PIXBUF_PIXEL(output, t->start_x, row);
 
-		width = input->w;
+		width = t->end_x - t->start_x;
 
-		while(width--)
+		while(width-- > 0)
 		{
 			r =
 				( i[R] * mati.coeff[0][0]
@@ -208,11 +235,14 @@ transform8_c(RS_IMAGE16 *input, GdkPixbuf *output, RS_MATRIX3 *matrix, guchar *t
 			o[R] = table8[r];
 			o[G] = table8[g];
 			o[B] = table8[b];
+			o[3] = 255;
+
 			i += input->pixelsize;
 			o += o_channels;
 		}
 	}
 }
+
 
 static void
 transform16_c(gushort* __restrict input, gushort* __restrict output, gint num_pixels, const gint pixelsize, RS_MATRIX3 *matrix)
@@ -301,8 +331,65 @@ convert_colorspace16(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *in
 	return TRUE;
 }
 
+gpointer
+start_single_cs8_transform_thread(gpointer _thread_info)
+{
+	ThreadInfo* t = _thread_info;
+	RS_IMAGE16 *input_image = t->input; 
+	GdkPixbuf *output = (GdkPixbuf*) t->output;
+	RSColorSpace *input_space = t->input_space;
+	RSColorSpace *output_space = t->output_space;
+
+	g_assert(RS_IS_IMAGE16(input_image));
+	g_assert(GDK_IS_PIXBUF(output));
+	g_assert(RS_IS_COLOR_SPACE(input_space));
+	g_assert(RS_IS_COLOR_SPACE(output_space));
+
+	gboolean sse2_available = (!!(rs_detect_cpu_features() & RS_CPU_FLAG_SSE2)) && cst_has_sse2();
+
+	if (sse2_available && rs_color_space_new_singleton("RSSrgb") == output_space)
+	{
+		transform8_srgb_sse2(t);
+		return (NULL);
+	}
+	if (sse2_available && rs_color_space_new_singleton("RSAdobeRGB") == output_space)
+	{
+		t->output_gamma = 1.0 / 2.19921875;
+		transform8_otherrgb_sse2(t);
+		return (NULL);
+	}
+	if (sse2_available && rs_color_space_new_singleton("RSProphoto") == output_space)
+	{
+		t->output_gamma = 1.0 / 1.8;
+		transform8_otherrgb_sse2(t);
+		return (NULL);
+	}
+	
+	/* Fall back to C-functions */
+	/* Calculate our gamma table */
+	const RS1dFunction *input_gamma = rs_color_space_get_gamma_function(input_space);
+	const RS1dFunction *output_gamma = rs_color_space_get_gamma_function(output_space);
+	guchar table8[65536];
+	gint i;
+	for(i=0;i<65536;i++)
+	{
+		gdouble nd = ((gdouble) i) * (1.0/65535.0);
+
+		nd = rs_1d_function_evaluate_inverse(input_gamma, nd);
+		nd = rs_1d_function_evaluate(output_gamma, nd);
+
+		/* 8 bit output */
+		gint res = (gint) (nd*255.0 + 0.5f);
+		_CLAMP255(res);
+		table8[i] = res;
+	}
+	t->table8 = table8;
+	transform8_c(t);
+	return (NULL);
+}
+
 static void
-convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *input_image, GdkPixbuf *output_image, RSColorSpace *input_space, RSColorSpace *output_space)
+convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *input_image, GdkPixbuf *output_image, RSColorSpace *input_space, RSColorSpace *output_space, GdkRectangle *_roi)
 {
 	g_assert(RS_IS_IMAGE16(input_image));
 	g_assert(GDK_IS_PIXBUF(output_image));
@@ -312,6 +399,16 @@ convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *inp
 	/* A few sanity checks */
 	g_assert(input_image->w == gdk_pixbuf_get_width(output_image));
 	g_assert(input_image->h == gdk_pixbuf_get_height(output_image));
+
+	GdkRectangle *roi = _roi;
+	if (!roi) 
+	{
+		roi = g_new(GdkRectangle, 1);
+		roi->x = 0;
+		roi->y = 0;
+		roi->width = input_image->w;
+		roi->height = input_image->h;
+	}
 
 	/* If a CMS is needed, do the transformation using LCMS */
 	if (RS_COLOR_SPACE_REQUIRES_CMS(input_space) || RS_COLOR_SPACE_REQUIRES_CMS(output_space))
@@ -332,34 +429,45 @@ convert_colorspace8(RSColorspaceTransform *colorspace_transform, RS_IMAGE16 *inp
 	{
 		const RS_MATRIX3 a = rs_color_space_get_matrix_from_pcs(input_space);
 		const RS_MATRIX3 b = rs_color_space_get_matrix_to_pcs(output_space);
+		
 		RS_MATRIX3 mat;
 		matrix3_multiply(&b, &a, &mat);
 
-		/* Calculate our gamma table */
-		/* FIXME: Move this to someplace where we can cache it! */
-		guchar table8[65536];
 		gint i;
+		guint y_offset, y_per_thread, threaded_h;
+		const guint threads = rs_get_number_of_processor_cores();
+		ThreadInfo *t = g_new(ThreadInfo, threads);
 
-		const RS1dFunction *input_gamma = rs_color_space_get_gamma_function(input_space);
-		const RS1dFunction *output_gamma = rs_color_space_get_gamma_function(output_space);
+		threaded_h = roi->height;
+		y_per_thread = (threaded_h + threads-1)/threads;
+		y_offset = roi->y;
 
-		for(i=0;i<65536;i++)
+		for (i = 0; i < threads; i++)
 		{
-			gdouble nd = ((gdouble) i) * (1.0/65535.0);
+			t[i].input = input_image;
+			t[i].output = output_image;
+			t[i].start_y = y_offset;
+			t[i].start_x = roi->x;
+			t[i].end_x = roi->x + roi->width;
+			t[i].cst = colorspace_transform;
+			t[i].input_space = input_space;
+			t[i].output_space = output_space;
+			y_offset += y_per_thread;
+			y_offset = MIN(input_image->h, y_offset);
+			t[i].end_y = y_offset;
+			t[i].matrix = &mat;
+			t[i].table8 = NULL;
 
-			nd = rs_1d_function_evaluate_inverse(input_gamma, nd);
-			nd = rs_1d_function_evaluate(output_gamma, nd);
-
-			/* 8 bit output */
-			gint res = (gint) (nd*255.0 + 0.5f);
-			_CLAMP255(res);
-			table8[i] = res;
+			t[i].threadid = g_thread_create(start_single_cs8_transform_thread, &t[i], TRUE, NULL);
 		}
 
-		transform8_c(
-			input_image,
-			output_image,
-			&mat,
-			table8);
+		/* Wait for threads to finish */
+		for(i = 0; i < threads; i++)
+			g_thread_join(t[i].threadid);
+
+		g_free(t);
 	}
+	/* If we created the ROI here, free it */
+	if (!_roi) 
+		g_free(roi);
 }
