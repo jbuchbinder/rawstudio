af11876e23b3ee9a4d6bec198dd2839c6d9944c2
akv <akv@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2012-04-29 21:28:05 +0000

    Using 8bit tiff output
    git-svn-id: https://rawstudio.org/svn/rawstudio/branches/4175-enfuse@4213 ff961bb0-e316-0410-bf66-ce93e2ff3b84

--- src/rs-enfuse.c
+++ src/rs-enfuse.c
@@ -133,7 +133,7 @@ GList * export_images(RS_BLOB *rs, GList *files, gboolean extend, gint dark, gfl
   if (g_object_class_find_property(G_OBJECT_GET_CLASS(output), "uncompressed"))
     g_object_set(output, "uncompressed", FALSE, NULL);
   if (g_object_class_find_property(G_OBJECT_GET_CLASS(output), "save16bit"))
-    g_object_set(output, "save16bit", TRUE, NULL);
+    g_object_set(output, "save16bit", FALSE, NULL); /* We get odd results if we use 16 bit output */
   if (g_object_class_find_property(G_OBJECT_GET_CLASS(output), "copy-metadata"))
     g_object_set(output, "copy-metadata", FALSE, NULL);
 
